65dceddcf416c08732b10082e6f74d3cebdc7b6c
aloks-thrymr <alok.s@thrymr.net>
2017-08-27 17:38:14 +0530

    commit1

---
+++ CommitCodeBean.java
@@ -0,0 +1,23 @@
+package net.thrymr.beans;
+
+import java.util.Date;
+
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import net.thrymr.models.AppUser;
+import net.thrymr.models.Project;
+
+public class CommitCodeBean {
+public String commitId;
+	
+
+	public String appUseremail;
+	public String projectname;
+	
+	public String commitMessage;
+	
+	public Date commitDate;
+
+
+}
